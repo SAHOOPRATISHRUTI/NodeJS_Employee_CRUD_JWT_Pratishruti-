efb2fd61e319bd50bb7794b82b83fdc82001ebc8
Pratishruti Sahoo <pratishrutisahoo7@gmail.com>
2024-08-22 18:42:28 +0530

    employeeController added

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -17,10 +17,56 @@ const createEmployee = async(req,res)=>{
         return Response.errorResponse(req,res,error)
 
     }
-   
+
+}
+
+const viewSinglEmployee= async(req,res)=>{
+    try{
+        const result= await employeeService.viewSingleEmployee(req.params.id)
+        console.log(result);
+        if(!result){
+            return Response.failResponse(req,res,null,messages.recordsFound,200)
+        }
+        return Response.successResponse(req,res,result,messages.successResponse,201)
+        
+    }catch(error){
+        return Response.errorResponse(req,res,error)
+    }
+}
+
+const editEmployee = async(req,res)=>{
+    try{
+        const result = await employeeService.editEmployee(req.params.id,req.body)
+        console.log(result);
+        if(!result){
+            return Response.failResponse(req,res,null,messages.failResponse,200)
+        }
+        if(result?.isduplicateEmail){
+            return Response.failResponse(req,res,null,messages.duplicateEmail,201)
+        }
+        return Response.successResponse(req,res,result,messages.successResponse,201)
+    }catch(error){
+        return Response.errorResponse(req,res,error)
+    }
+}
+
+const deleteEmployee = async(req,res)=>{
+    try{
+        const result = await employeeService.deleteEmployee(req.params.id)
+        console.log(result);
+        if(!result){
+            return Response.failResponse(req,res,null,messages.deleteFailedRecordNotFound,200)
+        }
+        return Response.successResponse(req,res,result,messages.deleteSuccess,201)
+        
+    }catch(error){
+        return Response.errorResponse(req,res,error)
+    }
     
 }
 
 module.exports={
-    createEmployee
+    createEmployee,
+    viewSinglEmployee,
+    editEmployee
 }
