0f33c071ca7ae80d202aa59be5da3899be331285
Pratishruti Sahoo <pratishrutisahoo7@gmail.com>
2024-08-22 10:47:57 +0530

    model added

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,3 +1,61 @@
+const { required } = require('joi')
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+const employeeSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        index:true,
+        trim:true
+    },
+    email: {
+        type: String,
+        // validate:{
+        //     validator: validator.isEmail,
+        //     message: '{VALUE} is not a valid email. Please enter a valid email'
+        // },
+        required: true,
+        default: null,
+        unique: true,
+        index: true 
+    },
+    password:{
+        type: String,
+        required: true,
+    } ,
+    otp:{
+        type: String
+    },
+    otpExpiry:{
+        type: Date
+    },
+    designation:{
+        type: String,
+        default: null
+    },
+    profileImage: {
+        type: String,
+        default: null,
+    },
+    department: {
+        type: String,
+        default: null
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    },
+    isActive : {
+        type: Boolean,
+        default: true,
+        index: true,
+    }  
+},{timestamps:true})
+
+const Employee = mongoose.model('Employee',employeeSchema)
+
+module.exports={
+    Employee
+}
